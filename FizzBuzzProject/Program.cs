d672ef92a83ca8d4d2318de685babc5663920a72
Kenneth Okalang <okalang.ok@gmail.com>
2023-12-06 20:16:48 +0100

    Update with comments

--- FizzBuzzProject/Program.cs
+++ FizzBuzzProject/Program.cs
@@ -6,16 +6,20 @@ namespace FizzBuzzProject
     {
         static void Main(string[] args)
         {
+            // Loop from 1 to 100
             for (int i = 1; i <= 100; i++)
             {
+                // If the number is divisible by 3 and 5, print FizzBuzz
                 if (i % 3 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine("FizzBuzz");
                 }
+                // If the number is divisible by 3, print Fizz
                 else if (i % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
                 }
+                // If the number is divisible by 5, print Buzz
                 else if (i % 5 == 0)
                 {
                     Console.WriteLine("Buzz");
